f8574b50ac4605ae8449e63fc20497d8595d7eee
YoonHyunWoo <yjs88zerg@gmail.com>
2022-03-03 16:23:46 +0900

    routing

---
+++ createtoken.js
@@ -0,0 +1,31 @@
+const jwt = require('jsonwebtoken');
+const express = require('express');
+const app = express();
+const router = express.Router();
+const SECRET_KEY = 'MY-SECRET-KEY';
+
+
+// POST /login 요청 body에 id와 password를 함께 실어서 요청으로 가정 (사실 id와 password는 암호화 되어있음)
+router.post('/login', (req, res, next) => {
+
+  //받은 요청의 id와 password로 DB에서 프로필사진, 닉네임 등 로그인 정보를 가져온다.
+  const nickname = "Yoon Hyun Woo";
+
+  //jwt.sign(payload, secretOrPrivateKey, [options, callback])
+  token = jwt.sign({
+    type: 'JWT',
+    nickname: nickname,
+  }, SECRET_KEY, {
+    expiresIn: '15m', // 만료시간 15분
+    issuer: 'yhw',
+  });
+
+  //response
+  return res.status(200).json({
+    code: 200,
+    message: '토큰이 발급되었습니다.',
+    token: token
+  });
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 const express = require('express')
 const app = express();
-const jwt = require('jsonwebtoken');
-app.get('/', (req,res)=>{
-    res.send('Hello World!');
+const router = express.Router();
+const createtoken = require('./createtoken.js');
 
-    var token = jwt.sign(
-    {
-        test: "test" //payload
-    },
-    "secretkey", //secreate key
-    {
-        subject: "PangYeon jwtToken",  // opriotns
-        expiresIn: '60m',              // 토큰에 대한 여러가지 정보를 설정한다.
-        issuer: "pangyeon"             // expiresin, issuerm, subject
-    }
-    );
-    console.log("토큰생성 :", token)
-})
+app.use('/', createtoken);
 
-
+app.use('/')
 app.listen(3000,()=>{
-    console.log("Server is running to localhost:3000..");
+    console.log("server is running to http://localhost:3000");
 })
