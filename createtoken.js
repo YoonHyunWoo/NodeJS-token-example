7352bf0e5d71b5cde554a9e0c3276c7888362191
yoonhyunwoo <yjs88zerg@gmail.com>
2022-03-04 15:18:24 +0900

    yhw

--- createtoken.js
+++ createtoken.js
@@ -2,7 +2,8 @@ const jwt = require('jsonwebtoken');
 const express = require('express');
 const app = express();
 const router = express.Router();
-const SECRET_KEY = 'MY-SECRET-KEY';
+  const secretKey = require('./config/jwt')
+const SECRET_KEY = secretKey;
 
 
 // POST /login 요청 body에 id와 password를 함께 실어서 요청으로 가정 (사실 id와 password는 암호화 되어있음)
@@ -16,15 +17,16 @@ router.post('/login', (req, res, next) => {
     type: 'JWT',
     nickname: nickname,
   }, SECRET_KEY, {
-    expiresIn: '15m', // 만료시간 15분
+     // 만료시간 15분
     issuer: 'yhw',
   });
 
   //response
+  res.cookie('jwt',token)
   return res.status(200).json({
     code: 200,
     message: '토큰이 발급되었습니다.',
-    token: token
+    jwt: token
   });
 });
 
--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express')
 const app = express();
 const router = express.Router();
 const createtoken = require('./createtoken.js');
+const jwt = require('jsonwebtoken');
+const web = require('./web/web')
+const verify = require('./verify')
 
+app.use('/', web);
 app.use('/', createtoken);
+app.use('/verify', verify)
 
-app.use('/')
+
 app.listen(3000,()=>{
     console.log("server is running to http://localhost:3000");
 })
+
